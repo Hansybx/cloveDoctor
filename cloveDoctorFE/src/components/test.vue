9ccabe4ff733ae086b039f60b9fd3f3e69448571
hansybx <1579450419@qq.com>
2022-03-12 16:24:16 +0800

    前端框架搭建

---
+++ cloveDoctorFE/src/components/test.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <el-button>Default</el-button>
+    <el-button :icon="Search" circle></el-button>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import {
+  Search
+} from '@element-plus/icons-vue'
+</script>
+
+<style>
+</style>
