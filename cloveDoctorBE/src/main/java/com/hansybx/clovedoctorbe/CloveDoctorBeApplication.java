a7b3a81ad3e4871ffb222b71de25176c5195e342
hansybx <1579450419@qq.com>
2022-03-22 16:18:54 +0800

    backend init

---
+++ cloveDoctorBE/src/main/java/com/hansybx/clovedoctorbe/CloveDoctorBeApplication.java
@@ -0,0 +1,13 @@
+package com.hansybx.clovedoctorbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloveDoctorBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloveDoctorBeApplication.class, args);
+    }
+
+}
---
+++ cloveDoctorBE/src/test/java/com/hansybx/clovedoctorbe/CloveDoctorBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hansybx.clovedoctorbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloveDoctorBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
