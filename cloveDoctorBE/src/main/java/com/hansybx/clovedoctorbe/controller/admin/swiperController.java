5ebb56d4d26b6319c9f311917d5a521e5febcf97
hansybx <1579450419@qq.com>
2022-03-24 21:47:03 +0800

    新增 获取/删除/批量删除/编辑轮播图功能

--- cloveDoctorBE/src/main/java/com/hansybx/clovedoctorbe/controller/admin/swiperController.java
+++ cloveDoctorBE/src/main/java/com/hansybx/clovedoctorbe/controller/admin/swiperController.java
@@ -2,6 +2,7 @@ package com.hansybx.clovedoctorbe.controller.admin;
 
 import com.hansybx.clovedoctorbe.DTO.SwiperDTO;
 import com.hansybx.clovedoctorbe.common.CommonResult;
+import com.hansybx.clovedoctorbe.model.Swiper;
 import com.hansybx.clovedoctorbe.service.admin.SwiperService;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +16,27 @@ public class swiperController {
     private SwiperService swiperService;
 
     @PostMapping("/swiper")
-    public CommonResult addSwiper(@RequestBody SwiperDTO swiperDTO){
+    public CommonResult addSwiper(@RequestBody SwiperDTO swiperDTO) {
         return swiperService.addSwiper(swiperDTO);
+    }
+
+    @GetMapping("/swiper")
+    public CommonResult getSwiperListPage(@RequestParam Integer pageNum, @RequestParam Integer pageSize) {
+        return swiperService.getSwiperListPage(pageNum, pageSize);
+    }
+
+    @PostMapping("/swiper/update")
+    public CommonResult updateSwiper(@RequestBody Swiper swiperDTO) {
+        return swiperService.updateSwiper(swiperDTO);
+    }
+
+    @PostMapping("/swiper/del")
+    public CommonResult swiperDel(@RequestBody SwiperDTO swiperDTO) {
+        return swiperService.delSwiper(swiperDTO);
+    }
+
+    @PostMapping("/swiper/del/multi")
+    public CommonResult swiperDelMulti(@RequestBody SwiperDTO[] swiperDTOs) {
+        return swiperService.delSwiperMulti(swiperDTOs);
     }
 }
--- cloveDoctorBE/src/main/java/com/hansybx/clovedoctorbe/service/admin/SwiperService.java
+++ cloveDoctorBE/src/main/java/com/hansybx/clovedoctorbe/service/admin/SwiperService.java
@@ -1,19 +1,29 @@
 package com.hansybx.clovedoctorbe.service.admin;
 
+import com.github.pagehelper.PageHelper;
 import com.hansybx.clovedoctorbe.DTO.SwiperDTO;
 import com.hansybx.clovedoctorbe.common.CommonResponse;
 import com.hansybx.clovedoctorbe.common.CommonResult;
 import com.hansybx.clovedoctorbe.mapper.SwiperMapper;
 import com.hansybx.clovedoctorbe.model.Swiper;
+import com.hansybx.clovedoctorbe.model.SwiperExample;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
 
 @Service
 public class SwiperService {
     @Resource
     SwiperMapper swiperMapper;
-    public CommonResult addSwiper(SwiperDTO swiperDTO){
+
+    public CommonResult addSwiper(SwiperDTO swiperDTO) {
+        SwiperExample swiperExample = new SwiperExample();
+        swiperExample.createCriteria().andDrugIdEqualTo(swiperDTO.getDrugId());
+        if (swiperMapper.countByExample(swiperExample) > 0) {
+            return CommonResponse.Fail("该药品已经配置过了");
+        }
         Swiper swiper = new Swiper();
         swiper.setAddTime(swiperDTO.getAddTime());
         swiper.setDrugId(swiperDTO.getDrugId());
@@ -22,4 +32,41 @@ public class SwiperService {
         swiperMapper.insert(swiper);
         return CommonResponse.Success();
     }
+
+    public CommonResult getSwiperListPage(Integer pageNum, Integer pageSize) {
+        String orderBy = "sortId desc";
+        PageHelper.startPage(pageNum, pageSize, orderBy);
+
+        SwiperExample swiperExample = new SwiperExample();
+        swiperExample.createCriteria();
+        List<Swiper> swiperList = swiperMapper.selectByExample(swiperExample);
+        Long totalNum = swiperMapper.countByExample(swiperExample);
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("totalNum", totalNum);
+        map.put("swiperList", swiperList);
+        return CommonResponse.Success(map);
+    }
+
+    public CommonResult updateSwiper(Swiper swiperDTO) {
+        SwiperExample swiperExample = new SwiperExample();
+        swiperExample.createCriteria().andIdEqualTo(swiperDTO.getId());
+        swiperMapper.updateByExample(swiperDTO, swiperExample);
+        return CommonResponse.Success();
+    }
+
+    public CommonResult delSwiper(SwiperDTO swiperDTO) {
+        SwiperExample swiperExample = new SwiperExample();
+        swiperExample.createCriteria().andIdEqualTo(swiperDTO.getId());
+        swiperMapper.deleteByExample(swiperExample);
+        return CommonResponse.Success();
+    }
+
+    public CommonResult delSwiperMulti(SwiperDTO[] swiperDTOs) {
+        for (SwiperDTO swiperDTO : swiperDTOs) {
+            swiperMapper.deleteByPrimaryKey(swiperDTO.getId());
+        }
+        return CommonResponse.Success();
+    }
+
 }
