248fe71d8292ec2b3208e9be0c14524732d648f4
hansybx <1579450419@qq.com>
2022-04-13 01:41:37 +0800

    管理员用户管理 后端接口

--- cloveDoctorBE/src/main/java/com/hansybx/clovedoctorbe/controller/admin/AdminUserController.java
+++ cloveDoctorBE/src/main/java/com/hansybx/clovedoctorbe/controller/admin/AdminUserController.java
@@ -1,4 +1,36 @@
 package com.hansybx.clovedoctorbe.controller.admin;
 
+import com.hansybx.clovedoctorbe.DTO.UserDTO;
+import com.hansybx.clovedoctorbe.common.CommonResult;
+import com.hansybx.clovedoctorbe.service.admin.AdminUserService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/v1/admin/user")
 public class AdminUserController {
+    @Resource
+    AdminUserService adminUserService;
+
+    @GetMapping("")
+    public CommonResult getTradeList(@RequestParam Integer pageNum, @RequestParam Integer pageSize) {
+        return adminUserService.getUserList(pageNum, pageSize);
+    }
+
+    @GetMapping("/search")
+    public CommonResult searchTradeList(@RequestParam String keywords, @RequestParam Integer pageNum, @RequestParam Integer pageSize) {
+        return adminUserService.searchUser(keywords, pageNum, pageSize);
+    }
+
+    @PostMapping("/change/type")
+    public CommonResult changeType(@RequestBody UserDTO userDTO) {
+        return adminUserService.changeType(userDTO);
+    }
+
+    @PostMapping("/del")
+    public CommonResult userDel(@RequestBody UserDTO userDTO) {
+        return adminUserService.userDel(userDTO);
+    }
 }
--- cloveDoctorBE/src/main/java/com/hansybx/clovedoctorbe/service/admin/AdminUserService.java
+++ cloveDoctorBE/src/main/java/com/hansybx/clovedoctorbe/service/admin/AdminUserService.java
@@ -1,4 +1,64 @@
 package com.hansybx.clovedoctorbe.service.admin;
 
+import com.github.pagehelper.PageHelper;
+import com.hansybx.clovedoctorbe.DTO.UserDTO;
+import com.hansybx.clovedoctorbe.common.CommonResponse;
+import com.hansybx.clovedoctorbe.common.CommonResult;
+import com.hansybx.clovedoctorbe.mapper.UserMapper;
+import com.hansybx.clovedoctorbe.model.User;
+import com.hansybx.clovedoctorbe.model.UserExample;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
 public class AdminUserService {
+    @Resource
+    UserMapper userMapper;
+
+    public CommonResult getUserList(Integer pageNum, Integer pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        UserExample example = new UserExample();
+        example.createCriteria();
+        List<User> userList = userMapper.selectByExample(example);
+        Long num = userMapper.countByExample(example);
+        Map<String, Object> map = new HashMap<>();
+        map.put("userList", userList);
+        map.put("total", num);
+        return CommonResponse.Success(map);
+    }
+
+    public CommonResult searchUser(String keyword, Integer pageNum, Integer pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        UserExample example = new UserExample();
+        example.createCriteria().andUsernameLike("%" + keyword + "%");
+        List<User> userList = userMapper.selectByExample(example);
+        Long num = userMapper.countByExample(example);
+        Map<String, Object> map = new HashMap<>();
+        map.put("userList", userList);
+        map.put("total", num);
+        return CommonResponse.Success(map);
+    }
+
+    public CommonResult changeType(UserDTO userDTO) {
+        User user = new User();
+        user.setId(userDTO.getId());
+        user.setPassword(userDTO.getPassword());
+        user.setType(userDTO.getType());
+        user.setUsername(userDTO.getUsername());
+        userMapper.updateByPrimaryKey(user);
+        return CommonResponse.Success();
+    }
+
+    public CommonResult userDel(UserDTO userDTO) {
+
+        int res = userMapper.deleteByPrimaryKey(userDTO.getId());
+        if (res > 0) {
+            return CommonResponse.Success();
+        }
+        return CommonResponse.Fail("删除失败");
+    }
 }
